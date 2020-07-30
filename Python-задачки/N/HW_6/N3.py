837b865dc4d3c0d2921814665a3ab958230f4007
Sam <a-tretyakov@yandex.ru>
2020-07-30 09:41:55 +0300

    Добавил в папку Настины задачи 6 домашки

---
+++ Python-задачки/N/HW_6/N3.py
@@ -0,0 +1,23 @@
+"""
+Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname, position (должность),
+income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы:
+оклад и премия, например, {"wage": wage, "bonus": bonus}. Создать класс Position (должность) на базе класса Worker.
+В классе Position реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учетом
+премии (get_total_income). Проверить работу примера на реальных данных (создать экземпляры класса Position,
+передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+"""
+
+
+class Worker:
+    name = None
+    surname = None
+    position = None
+    __income = {'wage': None, 'bonus': None}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        pass
+
+    def get_total_income(self):
+        pass
